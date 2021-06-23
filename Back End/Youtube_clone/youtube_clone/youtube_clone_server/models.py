a84edf9eb25382959dcc9437c2b030f5b6a1fcfd
Mike Edwards <michael.edwards.4108@gmail.com>
2021-06-23 17:05:01 -0400

    Added SubComment class, refactored Comments class, and added SubCommentSerializer to serializers.py.

--- Back End/Youtube_clone/youtube_clone/youtube_clone_server/models.py
+++ Back End/Youtube_clone/youtube_clone/youtube_clone_server/models.py
@@ -3,5 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Comments(models.Model):
-    videoID = models.IntegerField()
+    commentID = models.CharField(max_length=50)
+    videoID = models.CharField(max_length=50)
     initial_comment = models.TextField(max_length=500)
+
+
+class SubComment(models.Model):
+    sub_commentID = models.CharField(max_length=50)
+    commentID = models.ForeignKey(Comments, blank=True, null=True, on_delete=models.CASCADE)
+    sub_comment = models.TextField(max_length=500)
--- Back End/Youtube_clone/youtube_clone/youtube_clone_server/serializers.py
+++ Back End/Youtube_clone/youtube_clone/youtube_clone_server/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Comments
+from .models import Comments, SubComment
+
 
 class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comments
         fields = ['id', 'videoID', 'initial_comment']
+
+
+class SubCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubComment
+        fields = ['id', 'sub_commentID', 'commentID', 'sub_comment']
