20f00baf631d67b66145c8b3ef1818154443e01d
Mega Farts <clint.m.thompson@outlook.com>
2021-06-24 13:18:45 -0500

    Added Thumbnails, Title and description to videos

--- Front End/youtube_clone/src/App.js
+++ Front End/youtube_clone/src/App.js
@@ -12,16 +12,20 @@ class App extends Component {
     this.state = { 
       videoId: 'tgbNymZ7vqY',
       results: [],
+      title: [],
+      description: []
      }
   }
 
   async getVideo(searchValue){
-    let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchValue}&maxResults=8&type=video&key=AIzaSyBjCT6gMd3j2uFMkfikqK7f4YNUIy0xjlE`);
+    let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchValue}&maxResults=8&type=video&key=AIzaSyBjCT6gMd3j2uFMkfikqK7f4YNUIy0xjlE`);
     console.log(response.data)
 
     this.setState({
-      videoId: response.data.items[0].id.videoId,
-      results: [response.data.items[0].id.videoId, response.data.items[1].id.videoId, response.data.items[2].id.videoId, response.data.items[3].id.videoId, response.data.items[4].id.videoId, response.data.items[5].id.videoId, response.data.items[6].id.videoId, response.data.items[7].id.videoId]
+      results: [response.data.items[0].snippet.thumbnails.high.url, response.data.items[1].snippet.thumbnails.high.url, response.data.items[2].snippet.thumbnails.high.url, response.data.items[3].snippet.thumbnails.high.url, response.data.items[4].snippet.thumbnails.high.url, response.data.items[5].snippet.thumbnails.high.url, response.data.items[6].snippet.thumbnails.high.url, response.data.items[7].snippet.thumbnails.high.url],
+      videoId: [response.data.items[0].id.videoId, response.data.items[1].id.videoId, response.data.items[2].id.videoId, response.data.items[3].id.videoId, response.data.items[4].id.videoId, response.data.items[5].id.videoId, response.data.items[6].id.videoId, response.data.items[7].id.videoId],
+      title: [response.data.items[0].snippet.title, response.data.items[1].snippet.title, response.data.items[2].snippet.title, response.data.items[3].snippet.title, response.data.items[4].snippet.title, response.data.items[5].snippet.title, response.data.items[6].snippet.title, response.data.items[7].snippet.title],
+      description: [response.data.items[0].snippet.description, response.data.items[1].snippet.description, response.data.items[2].snippet.description, response.data.items[3].snippet.description, response.data.items[4].snippet.description, response.data.items[5].snippet.description, response.data.items[6].snippet.description, response.data.items[7].snippet.description]
    })
 
   }
@@ -35,7 +39,7 @@ class App extends Component {
       <React.Fragment>
         <SearchBar getVideo={this.getVideo}  results={this.state.results}/>
         <br></br>
-        <Results results={this.state.results}/>
+        <Results results={this.state.results} videoId={this.state.videoId} title={this.state.title} description={this.state.description}/>
       </React.Fragment>
      );
   }
--- Front End/youtube_clone/src/Components/SearchResults.jsx
+++ Front End/youtube_clone/src/Components/SearchResults.jsx
@@ -2,74 +2,65 @@ import React from 'react';
 
 const Results = (props) => {
 
+
+
     function displayResults(){
         console.log(props.results)
-        let displayResults = (<React.Fragment>     
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/${props.results[0]}`}>
-        </iframe>
-        
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/${props.results[1]}`}>
-        </iframe>
-        
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/${props.results[2]}`}>
-        </iframe>
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/${props.results[3]}`}>
-        </iframe>
+        let displayResults = (
+        <div style={{display: 'grid'}}>     
+            
+            <h2 style={{width: '25%'}}>{props.title[0]}</h2>
+            <img style={{width: '25%'}} src={`${props.results[0]}`} alt="video" />
+            <p style={{width: '25%'}}>{props.description[0]}</p>
+            
 
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/${props.results[4]}`}>
-        </iframe>
-        
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/${props.results[5]}`}>
-        </iframe>
-        
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/${props.results[6]}`}>
-        </iframe>
+            
+            <h2 style={{width: '25%'}}> {props.title[1]}</h2>
+            <img style={{width: '25%'}} src={`${props.results[1]}`} alt="video" />
+            <p style={{width: '25%'}}>{props.description[1]}</p>
+            
 
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/${props.results[7]}`}>
-        </iframe> 
-        </React.Fragment> )
+            
+            <h2 style={{width: '25%'}}>{props.title[2]}</h2>
+            <img style={{width: '25%'}} src={`${props.results[2]}`} alt="video" />
+            <p style={{width: '25%'}}>{props.description[2]}</p>
+            
+
+            
+            <h2 style={{width: '25%'}}>{props.title[3]}</h2>
+            <img style={{width: '25%'}} src={`${props.results[3]}`} alt="video" />
+            <p style={{width: '25%'}}>{props.description[3]}</p>
+            
+
+            
+            <h2 style={{width: '25%'}}>{props.title[4]}</h2>
+            <img style={{width: '25%'}} src={`${props.results[4]}`} alt="video" />
+            <p style={{width: '25%'}}>{props.description[4]}</p>
+            
+
+            
+            <h2 style={{width: '25%'}}>{props.title[5]}</h2>
+            <img style={{width: '25%'}} src={`${props.results[5]}`} alt="video" />
+            <p style={{width: '25%'}}>{props.description[5]}</p>
+            
+
+            
+            <h2 style={{width: '25%'}}>{props.title[6]}</h2>
+            <img style={{width: '25%'}} src={`${props.results[6]}`} alt="video" />
+            <p style={{width: '25%'}}>{props.description[6]}</p>
+            
+
+            
+            <h2 style={{width: '25%'}}>{props.title[7]}</h2>
+            <img style={{width: '25%'}} src={`${props.results[7]}`} alt="video" />
+            <p style={{width: '25%'}}>{props.description[7]}</p>
+            
+            
+        </div> )
+
         if (props.results == ''){
             return <React.Fragment>
 
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/M9v9k77DwoY`}>
-        </iframe>
-        
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/8F6meOljv-s`}>
-        </iframe>
-        
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/aoqrHE4Z938`}>
-        </iframe>
-
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/CtHwcfNHfPM`}>
-        </iframe>
-    
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/vBrnBmUmVzI`}>
-        </iframe>
-        
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/hYYDtRZ-rUA`}>
-        </iframe>
-        
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/t84ZS-2i1BM`}>
-        </iframe>
-
-        <iframe width="420" height="315" title="Main Video Player"
-        src={`https://www.youtube.com/embed/V17s91XeqvU`}>
-        </iframe>  
 
             </React.Fragment>
         }
